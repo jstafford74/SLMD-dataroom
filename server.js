22e377c07d6664fba1a889bbd9304af3f7c0de85
Jeremiah Stafford <52318268+jstafford74@users.noreply.github.com>
2020-01-22 09:27:49 -0500

    mongo connection

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/slmdusers";
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
 // Start the API server
