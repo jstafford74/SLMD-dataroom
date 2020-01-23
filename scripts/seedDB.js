62f8e226b1fc403048967fce851fb68bdbb427c4
Jeremiah Stafford <52318268+jstafford74@users.noreply.github.com>
2020-01-22 20:27:39 -0500

    package jspn fix

---
+++ scripts/seedDB.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+var bcrypt = require('bcryptjs');
+
+const db = require("../models");
+
+// This file empties the Book and User collections and inserts the seeds below
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+
+const demoUserSeed = [
+  {
+    role: "user",
+    firstName: "Jeremiah",
+    lastName: "Stafford",
+    email: "jstafford@slmdevice.com",
+    username: 'jstafford'
+  },
+  {
+    role: "user",
+    firstName: "George",
+    lastName: "Chen",
+    email: "gchen@slmdevice.com",
+    username: 'gchen'
+  }
+]
+
+
+
+async function seed() {
+  try {
+    // clear DB
+    
+    await db.User.remove({});
+    
+    // add demo users
+    const saltRounds = parseInt(process.env.PASSWORD_SALT_ROUNDS, 10);
+    const password = process.env.SEED_USER_PASSWORD;
+    await Promise.all(demoUserSeed.map(async (it) => {
+      it.passwordHash = await bcrypt.hash(password, saltRounds);
+      return;
+    }));
+
+    const userSeedOp = await db.User.collection.insertMany(demoUserSeed);
+    
+    
+
+    process.exit(0);
+
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+seed();
